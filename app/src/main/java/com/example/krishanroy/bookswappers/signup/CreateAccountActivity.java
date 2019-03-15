0791edec17c0f75daf6f359ed474f51e720e448f
krishanroy <krishanroy@pursuit.org>
2019-03-15 14:29:47 -0400

    initial commit

---
+++ app/src/main/java/com/example/krishanroy/bookswappers/signup/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.example.krishanroy.bookswappers.signup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.krishanroy.bookswappers.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+
+    }
+}
