d8e8c68c033f420ee40f2710cfec98bfbf038b6b
krishanroy <krishanroy@pursuit.org>
2019-03-26 14:49:09 -0400

    added keys to the model class

--- app/src/main/java/com/example/krishanroy/bookswappers/ui/model/Persons.java
+++ app/src/main/java/com/example/krishanroy/bookswappers/ui/model/Persons.java
@@ -3,6 +3,8 @@ package com.example.krishanroy.bookswappers.ui.model;
 public class Persons {
     private int id;
     private String name;
+    private String title;
+    private Address address;
 
     public int getId() {
         return id;
@@ -12,8 +14,57 @@ public class Persons {
         return name;
     }
 
-    public Persons(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public class Address {
+        private String street;
+        private String suite;
+        private String city;
+        private String zipcode;
+        private Geo geo;
+
+        public String getStreet() {
+            return street;
+        }
+
+        public String getSuite() {
+            return suite;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getZipcode() {
+            return zipcode;
+        }
+
+        public Geo getGeo() {
+            return geo;
+        }
+
+        public Address(String street, String suite, String city, String zipcode, Geo geo) {
+            this.street = street;
+            this.suite = suite;
+            this.city = city;
+            this.zipcode = zipcode;
+            this.geo = geo;
+        }
+
+        private class Geo {
+            private String lat;
+            private String lng;
+
+            public String getLat() {
+                return lat;
+            }
+
+            public String getLng() {
+                return lng;
+            }
+
+            public Geo(String lat, String lng) {
+                this.lat = lat;
+                this.lng = lng;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/krishanroy/bookswappers/ui/view/BookViewHolder.java
+++ app/src/main/java/com/example/krishanroy/bookswappers/ui/view/BookViewHolder.java
@@ -3,7 +3,10 @@ package com.example.krishanroy.bookswappers.ui.view;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.krishanroy.bookswappers.R;
 import com.example.krishanroy.bookswappers.ui.model.Persons;
 
 public class BookViewHolder extends RecyclerView.ViewHolder {
@@ -11,6 +14,10 @@ public class BookViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
     }
     public void onBind(Persons persons){
+        TextView bookTitleTextView = itemView.findViewById(R.id.book_title_textview);
+        ImageView bookCoverImageView = itemView.findViewById(R.id.book_cover_imageview);
+        TextView locationTextView = itemView.findViewById(R.id.location_textview);
+        bookTitleTextView.setText(persons.);
 
     }
 }
